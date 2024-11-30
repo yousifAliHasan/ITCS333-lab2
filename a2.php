f1ba0549e22d3f0bc1fe1f4475ee0ea2c51584d3
Mahmood Alawi <75913498+Mahmood11Alawi@users.noreply.github.com>
2024-11-30 03:03:45 +0300

    Update a2.php

--- a2.php
+++ a2.php
@@ -1,1 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University of Bahrain Students Enrollment by Nationality</title>
+    <link rel="stylesheet" href="https://unpkg.com/picocss/pico.min.css">
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
 
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #ffffff; /* white */
+        }
+
+        tbody tr:nth-child(odd) {
+            background-color: #f7f7f7; /* Light grey */
+        }
+
+        tbody tr:nth-child(even) {
+            background-color: #ffffff; /* white */
+        }
+
+        @media (max-width: 600px) {
+            th, td {
+                display: block;
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body>
+<main>
+</html>
